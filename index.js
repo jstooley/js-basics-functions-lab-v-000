989721b67afe7f4d7c7302028d9773953136cceb
Justin Tooley <jstooley823@gmail.com>
2018-02-14 02:15:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function distanceFromHqInFeet (block) {
 }
 
 function distanceTravelledInFeet (start,end) {
-  return (start - end) * 264;
+  return Math.abs(start - end) * 264;
 }
 
 function calculatesFarePrice (start, destination) {
