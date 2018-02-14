ab84bfa76b2f0813e0b3e940dbc588b1a46e1878
Justin Tooley <jstooley823@gmail.com>
2018-02-14 02:21:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function calculatesFarePrice (start, destination) {
 
   if (distanceOfFare < 400) {
     return 0;
-  } else if (distanceOfFare =< 2000 ) {
+  } else if (distanceOfFare =< 2000) {
     return distanceOfFare * 0.02;
   } else if (distanceOfFare > 2000 && distanceOfFare =< 2500 ){
     return 25;
