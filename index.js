e3d940e70db5837e6195863073a0735667ede900
Justin Tooley <jstooley823@gmail.com>
2018-02-14 01:56:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function distanceFromHqInBlocks (block) {
   let distance_in_blocks = 42 - block
 
   if (distance_in_blocks < 0){distance_in_blocks *= -1}
+
+  return distance_in_blocks
 }
 
 function distanceFromHqInFeet () {}
