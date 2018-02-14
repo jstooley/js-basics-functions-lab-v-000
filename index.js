0419d4bb74f7bd274c5890343e29fb8f01745e1a
Justin Tooley <jstooley823@gmail.com>
2018-02-14 02:20:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ function distanceTravelledInFeet (start,end) {
 function calculatesFarePrice (start, destination) {
   const distanceOfFare = distanceTravelledInFeet(start, destination);
 
-  
+  if (distanceOfFare < 400) {
+    return 0;
+  } else if (distanceOfFare =< 2000 ) {
+    return distanceOfFare * 0.02;
+  } else if (distanceOfFare > 2000 && distanceOfFare =< 2500 ){
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
 }
